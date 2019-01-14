d9a5422f25fcb6b68348f64f63b982a64da6689b
Michael McKay <mamckay89@gmail.com>
2019-01-14 10:08:57 -0700

    completed triangle problem

---
+++ triangular.js
@@ -0,0 +1,18 @@
+// 3 + 2 + 1
+// 2 + 1
+// 1
+
+// 4 3 2 1 = 10
+// 5 4 3 2 1 = 15
+// 6 5 4 3 2 1 = 21
+
+function triangle(x) {
+    if(x === 0){
+        return 0; 
+    }
+    else{
+        return x + triangle(x - 1); 
+    }
+}
+
+console.log(triangle(9));
